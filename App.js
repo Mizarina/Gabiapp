20169f7e604d567b676001472fc7ea314f2a32dd
Mizarina <86027970+Mizarina@users.noreply.github.com>
2022-12-14 17:08:11 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import { Button, Input, Text } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome'
-import styles from './style/MainStyle'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import primeira from './screens/primeira'
+import Home from './screens/Home'
+import Segunda from './screens/Segunda'
+import { StackNavigator} from 'react-navigation';
+import Terceira from './screens/Terceira'
+import Quarta from './screens/Quarta'
+import Quinta from './screens/Quinta'
 
 
- const Stack=[createStackNavigatior()] 
+
+ const Stack=[createNativeStackNavigatior()] 
  
- function MyStack(){
+ function App(){
   return(
+    <NavigationContainer>
     <Stack.Navigatior>
       <Stack.Screen name="Home"component={Home}/>
       <Stack.Screen name= "Segunda" component={Segunda}/>
@@ -20,17 +25,11 @@ import primeira from './screens/primeira'
       <Stack.Screen name= "Quinta" component={Quinta}/>
 
     </Stack.Navigatior>
-
-
+    </NavigationContainer>
 
   )
  }
 
 
-
-
-export default function App() {
-  
-  
-}
+export default App;
  
