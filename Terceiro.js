8870356c172c7267070ca560e8a483d7d7e92bdd
Mizarina <86027970+Mizarina@users.noreply.github.com>
2022-12-14 17:10:52 -0300

    Update Terceiro.js

--- Terceiro.js
+++ Terceiro.js
@@ -1,1 +1,27 @@
+import React, {useState} from 'react';
+import {Text, View, StatusBar} from 'react-native';
 
+export default function Terceira() {
+    return(
+        <View>
+            <Text> Vida Academica </Text>
+            <StatusBar style ="auto"/>
+
+            <Text> 
+            Já estudei no SESI no fundamental 1 e estudava no colégio Isaac Newton lá na serraria, fiz meu fundamental 2 completo e em 2021 ingressei no ifal.
+            
+            
+            </Text>
+
+        <Text> 
+        Ao ingressar no ifal participei da OALQ (Olimpiada Alagoana de Quimica e ganhei Menção Honrosa, no inicio de 2022 comecei a participar do Desafio inpiratech do Sebrae onde ficamos na colocação 50 do Nacional.</Text>
+
+        <Text>
+
+        Em Agosto participei do Hackhaton Pilar startup challenge que aconteceu durante o evento do Trakto show e partcipei junto com meus amigos e professores do IFAL, participando desses Desafios foi que percebi a necessidade de aprender Programação e iniciei o curso de React Native. </Text>
+
+        </View>
+    )
+
+    
+}
