802f21841f159cbc75ded441a2f1664f2692f3c2
Mizarina <86027970+Mizarina@users.noreply.github.com>
2022-12-14 17:10:11 -0300

    Update Segunda.js

--- Segunda.js
+++ Segunda.js
@@ -1,8 +1,32 @@
 import React, {useState} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StatusBar} from 'react-native';
 
 export default function Segunda() {
-    return
+    return(
+        <View>
+            <Text> O que eu Gosto? </Text>
+            <StatusBar style ="auto"/>
+
+            <Text> 
+            Eu gosto muito de aventura e de viajar, inclusive uma das minhas metas para o ano que vem é participar de várias trilhas.
+            </Text>
+
+
+            <Text> 
+            Gosto também de jogar vôlei, é o meu esporte preferido. 
+            
+            </Text>
+
+
+            <Text> 
+            
+            Gosto de estar com a minha família, de estudar, assistir séries e filmes, desenhar e de cozinhar.
+            </Text>
+
+
+
+        </View>
+    )
 
     
 }
