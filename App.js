8feb1415036691cc5d9f71d44b6e76f475488f74
Mizarina <86027970+Mizarina@users.noreply.github.com>
2022-12-19 22:04:21 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,35 +1,26 @@
-import React, {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
-import { Button, Input, Text } from 'react-native-elements';
+import {SafeAreaView, } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './screens/Home'
-import Segunda from './screens/Segunda'
-import { StackNavigator} from 'react-navigation';
-import Terceira from './screens/Terceira'
-import Quarta from './screens/Quarta'
-import Quinta from './screens/Quinta'
+import Home from './screens/Home';
+import Segunda from './screens/Segunda';
+import Terceira from './screens/Terceira';
+import Quarta from './screens/Quarta';
+import Quinta from './screens/Quinta';
 
 
+const Stack = createStackNavigator();
 
- const Stack=[createNativeStackNavigatior()] 
- 
- function App(){
+export default function App(){
   return(
     <NavigationContainer>
-    <Stack.Navigatior>
-      <Stack.Screen name="Home"component={Home}/>
-      <Stack.Screen name= "Segunda" component={Segunda}/>
-      <Stack.Screen name= "Terceira" component={Terceira}/>
-      <Stack.Screen name= "Quarta" component={Quarta}/>
-      <Stack.Screen name= "Quinta" component={Quinta}/>
-
-    </Stack.Navigatior>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Segunda" component={Segunda} />
+      <Stack.Screen name="Terceira" component={Terceira} />
+      <Stack.Screen name="Quarta" component={Quarta} />
+      <Stack.Screen name="Quinta" component={Quinta} />
+    </Stack.Navigator>
     </NavigationContainer>
-
-  )
+  );
  }
 
-
-export default App;
- 
