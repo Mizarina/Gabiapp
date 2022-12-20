6a254f5c717c431ca8be1554130f7bfba86bea58
Mizarina <86027970+Mizarina@users.noreply.github.com>
2022-12-19 22:05:09 -0300

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,47 +1,110 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React,  {useState} from 'react';
 import { StyleSheet, Text, View, Button, createElement, Image, SafeAreaView, TouchableOpacity, TouchableHighlight } from 'react-native';
-import Image from './assets/Fotogabi.png'
+
  
- 
-export default function Home() {
-  const [textoImagem, setTextoImagem,] = UseState("");
+export default function Home({navigation}) {
+  const [textoImagem, setTextoImagem,] = useState("");
   
     return (
       <View style={styles.container}>
-        
-      <Text style ={{color:"Black", 
-        fontSize: 80, 
-        margin: 20,}}>
-        GABI    
-      </Text>
 
-      <TouchableOpacity>
-      style= {styles.buttongabi}
-        <Image>
-          source= {require('./assets/Fotogabi.png')}
-          style= {styles.fotobuttongabi}
-        </Image>
-      </TouchableOpacity>
+       <View
+       style={{  backgroundColor: '#F4A460', margin: 10, width:'300px'
+      , height: '40px',justifyContent: 'center', borderRadius: 50
+       }}>
+       
+       <Text style={{ color :'white',
+            fontSize: 30,
+            fontStyle: 'font-style: obliquo',
+            fontWeight: 'bold',
+            fontFamily: 'Georgia',
+            textAlign: 'center',
+            overflow: "hidden",
+            borderRadius: 60,
+            margin: 5
+
+
+            }}> 
+        Welcome   
+        <StatusBar style="auto" />
+      </Text>
+       
+       
+       
+       
+       
+       
+       
+       </View>
 
 
       
- <StatusBar style="auto" />
+      
+  
 
       <TouchableHighlight onPress={() => setTextoImagem("Meu nome é Gabriela, tenho 17 anos, Sou do curso de Estradas e tenho interesse pela área de Tecnologia,Inovação e empreendedorismo.")}>
-        <Image> source={require('./assets/Fotogabi.png')}</Image>
+      
+
+        <View>
+       <Image source= {require('../assets/Fotogabi.png.png')}
+        style= {
+          {widht: '150px', 
+          height: '150px',
+          alignItems: 'center', 
+          justifyContent: 'center', 
+          borderRadius: 800,
+          resizeMode: 'contain',
+          margin: 10
+        }}
+        
+        />
+
+
+<Text style={{ color :'black',
+            fontSize: 25,
+            fontStyle: 'font-style: obliquo',
+            fontWeight: 'bold',
+            fontFamily: 'FreeMono',
+            textAlign: 'center',
+            overflow: "hidden",
+            borderRadius: 60,
+            margin: 5
+
+
+            }}> 
+        GABI    
+        <StatusBar style="auto" />
+      </Text>
+
+        </View>
+      
       </TouchableHighlight>
 
       <Text>{textoImagem}
       
       </Text>
+  
+   
+     <View style={{marginLeft: 250}}>
 
       <Button
         title="Next"
-        onPress={() => navigation.navigate('Segunda')} />
+        color = '#F4A460'
+       
+        
+        
 
-      </View>
+        onPress={() => navigation.navigate('Segunda')}
+         />
+        </View>
+         
+
+        </View>
+
     );
+
+
 }
   
   
@@ -50,27 +113,13 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#FFF8DC',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 5
 
-  },
-
-  logo:{
-    alignItems:"stretch"
   }
-  
-  ,
-  tecla:{
-  alignItems:"flex-end"
-  
-  }
-  
-});
+})
 
-  
-
-  
-  
 
   
