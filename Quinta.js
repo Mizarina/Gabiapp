858d52612891e9a22532603479fb057566ef7a0e
Mizarina <86027970+Mizarina@users.noreply.github.com>
2022-12-19 22:07:54 -0300

    Update Quinta.js

--- Quinta.js
+++ Quinta.js
@@ -1,44 +1,88 @@
-import React from 'react';
-import {Text, View, StatusBar,StyleSheet,Animated} from 'react-native';
-import Image from './assets/flork.gif'
+import React, {useState, useRef, useEffect} from 'react';
+import {Text, View, StatusBar,StyleSheet,Animated, Image,Button} from 'react-native';
 
-export default function Quinta() {
+
+
+export default function Quinta({navigation}) {
 
   const [largura,setLargura] = useState (new Animated.Value(0));
   const [altura,setAltura] = useState (new Animated.Value(30))
+  state = { height: new Animated.Value(0) };
 
-  Animated.sequence([
+  const fadeAnim = useRef(new Animated.Value(0)).current  // Initial value for opacity: 0
+  const zoomIn = useRef(new Animated.Value(0)).current
+
+  useEffect(() => {
     Animated.timing(
-      largura,
+      fadeAnim,
       {
-        toValue:420,
-        duration:2000
-      }
-    ),
-    Animated.timing(
-      altura,
-      {
-        toValue:500,
-        duration:1000,
+        toValue: 1,
+        duration: 3000,
       }
 
-    )
+    ).start();
+    
+  }, [fadeAnim])
 
-  ]).start();
-  
     
     return(
         <View style ={styles.container}>
         <Animated.View style={{
-          width: 370,
-          height: 30,
-          backgroundColor: "black"
+          
+          opacity: fadeAnim,
+
         }}>
+          <Image source={require('../assets/flork-gif.gif')}
+          style={{
+            width: "250px",
+            height: "250px",
+            }}
+          />
            </Animated.View>
-           <Image> source = require{('./assets/flork.gif')} style{{ widht: 200, height: 50}}</Image>
+          
+          <View
+       style={{  backgroundColor: '#F4A460', margin: 10, width:'300px'
+      , height: '40px',justifyContent: 'center', borderRadius: 50
+       }}>
+       
+       <Text style={{ color :'white',
+            fontSize: 30,
+            fontStyle: 'font-style: obliquo',
+            fontWeight: 'bold',
+            fontFamily: 'Georgia',
+            textAlign: 'center',
+            overflow: "hidden",
+            borderRadius: 60,
+            margin: 5
+
+
+            }}> 
+        THE END   
+        <StatusBar style="auto" />
+      </Text>
+       
+       
+       
+       
+       
+       
+       
+       </View>
+
+          
 
         </View>
     )
 
     
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF8DC',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  }
+})
