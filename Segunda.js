418438e2241490dfc0400e9ace0dc0d206d3d624
Mizarina <86027970+Mizarina@users.noreply.github.com>
2022-12-19 22:05:52 -0300

    Update Segunda.js

--- Segunda.js
+++ Segunda.js
@@ -1,32 +1,118 @@
-import React, {useState} from 'react';
-import {Text, View, StatusBar} from 'react-native';
+import { View, Text, StyleSheet, Button} from 'react-native'
+import React from 'react';
 
-export default function Segunda() {
-    return(
-        <View>
-            <Text> O que eu Gosto? </Text>
-            <StatusBar style ="auto"/>
 
-            <Text> 
-            Eu gosto muito de aventura e de viajar, inclusive uma das minhas metas para o ano que vem é participar de várias trilhas.
+
+
+ export default function Segunda  ({navigation}) {
+ 
+  return (
+    
+    <View style={styles.container}>
+      
+       <Text style={{ color :'black',
+             fontSize: 20,
+            fontStyle: 'font-style: obliquo',
+            fontWeight: 'bold',
+            fontFamily: 'Georgia',
+            textAlign: 'start',
+            margin: 10
+
+
+            }}> 
+            
+            O QUE EU GOSTO? </Text>
+    
+  
+
+      <View 
+      
+      style={{  backgroundColor: '#FFF8DC', margin: 10, width:'323px'
+      , height: '100px',justifyContent: 'center',
+       }}>
+
+      <Text style={{ color :'black',
+             fontSize: 13,
+            fontStyle: 'font-style: obliquo',
+            fontFamily: 'Georgia',
+            textAlign: 'center',
+            textBreakStrategy: 'simple',
+           
+            }}>
+            
+            
+             Eu gosto muito de aventura e de viajar, inclusive uma das minhas metas para o ano que vem é participar de várias trilhas.
+            
             </Text>
 
-
-            <Text> 
+       </View>
+      
+      
+      <View style={{  backgroundColor: '#FFF8DC', margin: 10, width:'323px',height: '100px', justifyContent: 'center', }}> 
+      <Text style={{color: 'black',
+             fontSize: 13,
+            fontStyle: 'font-style: obliquo',
+            fontFamily: 'Georgia',
+            textAlign: 'center',
+            textBreakStrategy: 'simple',
+           
+            }}> 
             Gosto também de jogar vôlei, é o meu esporte preferido. 
             
             </Text>
 
+      </View>
+    
+      
+      <View style={{  backgroundColor: '#FFF8DC' , margin: 10, width:'323px',height: '100px', justifyContent: 'center',}}>
 
-            <Text> 
+      <Text style={{color: 'black',
+             fontSize: 13,
+            fontStyle: 'font-style: obliquo',
+            fontFamily: 'Georgia',
+            textAlign: 'center',
+            textBreakStrategy: 'simple',
+           
+            }}> 
             
             Gosto de estar com a minha família, de estudar, assistir séries e filmes, desenhar e de cozinhar.
             </Text>
 
 
+      </View>
+    
+    <View style={{marginLeft: 250}}>
+    <Button 
+          title="Next"
+          color = '#F4A460'
+        justifyContent= 'space-between'
+        flexDirection= 'row'
+        onPress={() => navigation.navigate('Terceira')} />
+  
+    </View>
+    </View>
+    
+  
+  );
 
-        </View>
-    )
+};
+
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 5
+
+  }
+})
+
+
 
     
-}
+
